fe02813409ece9868ca53dbd6e77c9edff01b43c
jaeduck223 <145331405+jaeduck223@users.noreply.github.com>
2025-06-30 10:11:31 +0900

    Add files via upload
    교재 내용 그대로 베낀 거랑 다름 없어서 큰 의미 없이 복습용으로 업로드하는 것들이다.

---
+++ assignment.js
@@ -0,0 +1,13 @@
+//숫자 복합 대입 연산자 실습
+let output = 0;
+output += 1;
+output += 2;
+output += 3;
+
+console.log(output);
+//문자열 복합 대입 연산자 실습
+
+let input = "hello";
+input += "World";
+input += "!";
+console.log(input);
